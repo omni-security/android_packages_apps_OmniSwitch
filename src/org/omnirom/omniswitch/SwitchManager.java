cb2c13664c3e55513a508aff246e011316953579
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2014-03-28 22:59:23 +0700

    OmniSwitch: Allow open app as floating window
    Since i am using OmniSwitch to activate an app as
    floating window, i am recommitting this commit so it
    can be adjusted and rebased if needed.
    
    PS 6: Rebased:
          Conflicts: rc/org/omnirom/omniswitch/ui/SwitchGestureView.java
    
    Conflicts:
    	src/org/omnirom/omniswitch/SwitchManager.java
    
    Change-Id: If4dbb4c99113a4a51bc78eb3a4a1447bf8d22fda

--- src/org/omnirom/omniswitch/SwitchManager.java
+++ src/org/omnirom/omniswitch/SwitchManager.java
@@ -150,15 +150,21 @@ public class SwitchManager {
             hide(true);
         }
 
-        if (ad.getTaskId() >= 0) {
+        Intent intent = ad.getIntent();
+        boolean floating = (intent.getFlags() & Intent.FLAG_FLOATING_WINDOW) == Intent.FLAG_FLOATING_WINDOW;
+        if (ad.getTaskId() >= 0 && !floating) {
             // This is an active task; it should just go to the foreground.
             am.moveTaskToFront(ad.getTaskId(),
                     ActivityManager.MOVE_TASK_WITH_HOME);
         } else {
-            Intent intent = ad.getIntent();
-            intent.addFlags(Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY
-                    | Intent.FLAG_ACTIVITY_TASK_ON_HOME
-                    | Intent.FLAG_ACTIVITY_NEW_TASK);
+            if (!floating) {
+                intent.addFlags(Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY
+                       | Intent.FLAG_ACTIVITY_TASK_ON_HOME
+                       | Intent.FLAG_ACTIVITY_NEW_TASK);
+            } else {
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                       | Intent.FLAG_FLOATING_WINDOW);
+            }
             if (DEBUG)
                 Log.v(TAG, "Starting activity " + intent);
             try {
@@ -167,6 +173,27 @@ public class SwitchManager {
                 Log.e(TAG, "Recents does not have the permission to launch "
                         + intent, e);
             }
+        }
+    }
+
+    public void floatingTask(TaskDescription ad) {
+        if (ad.isKilled()) {
+            return;
+        }
+
+        hide(true);
+
+        Intent intent = ad.getIntent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY
+                    | Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_FLOATING_WINDOW);
+        if (DEBUG)
+                Log.v(TAG, "Starting activity " + intent);
+        try {
+             mContext.startActivity(intent);
+        } catch (SecurityException e) {
+             Log.e(TAG, "Recents does not have the permission to launch "
+                     + intent, e);
         }
     }
 
@@ -300,14 +327,18 @@ public class SwitchManager {
         TaskDescription ad = mLoadedTasks.get(1);
         switchTask(ad, close);
     }
-
-    public void startIntentFromtString(String intent, boolean close) {
+    
+    public void startIntentFromtString(String intent, boolean close, boolean float_window) {
         if(close){
             hide(true);
         }
 
         try {
             Intent intentapp = Intent.parseUri(intent, 0);
+            if(float_window) {
+                intentapp.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_FLOATING_WINDOW);
+            }
             mContext.startActivity(intentapp);
         } catch (URISyntaxException e) {
             Log.e(TAG, "URISyntaxException: [" + intent + "]");
--- src/org/omnirom/omniswitch/ui/SwitchLayout.java
+++ src/org/omnirom/omniswitch/ui/SwitchLayout.java
@@ -511,7 +511,7 @@ public class SwitchLayout implements OnShowcaseEventListener {
             public void onItemClick(AdapterView<?> parent,
                     View view, int position, long id) {
                 String intent = mFavoriteList.get(position);
-                mRecentsManager.startIntentFromtString(intent, true);
+                mRecentsManager.startIntentFromtString(intent, true, false);
             }
         });
         mFavoriteListHorizontal.setAdapter(mFavoriteListAdapter);
@@ -541,7 +541,7 @@ public class SwitchLayout implements OnShowcaseEventListener {
             public void onItemClick(AdapterView<?> parent, View view,
                     int position, long id) {
                 PackageManager.PackageItem packageItem = PackageManager.getInstance(mContext).getPackageList().get(position);
-                mRecentsManager.startIntentFromtString(packageItem.getIntent(), true);
+                mRecentsManager.startIntentFromtString(packageItem.getIntent(), true, false);
             }});
         mAppDrawer.setOnTouchListener(new OnTouchListener() {
             @Override
@@ -994,7 +994,9 @@ public class SwitchLayout implements OnShowcaseEventListener {
         popup.getMenu().findItem(R.id.recent_add_favorite).setEnabled(!mFavoriteList.contains(ad.getIntent().toUri(0)));
         popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             public boolean onMenuItemClick(MenuItem item) {
-                if (item.getItemId() == R.id.recent_remove_item) {
+                if (item.getItemId() == R.id.recent_float_item) {
+                    mRecentsManager.floatingTask(ad);
+                } else if (item.getItemId() == R.id.recent_remove_item) {
                     mRecentsManager.killTask(ad);
                 } else if (item.getItemId() == R.id.recent_inspect_item) {
                     mRecentsManager.startApplicationDetailsActivity(ad.getPackageName());
@@ -1039,6 +1041,8 @@ public class SwitchLayout implements OnShowcaseEventListener {
                     mRecentsManager.startApplicationDetailsActivity(packageItem.getActivityInfo().packageName);
                 } else if (item.getItemId() == R.id.recent_remove_favorite) {
                     Utils.removeFromFavorites(mContext, packageItem.getIntent(), mFavoriteList);
+                } else if (item.getItemId() == R.id.recent_float_item) {
+                    mRecentsManager.startIntentFromtString(packageItem.getIntent(), true, true);
                 } else {
                     return false;
                 }
